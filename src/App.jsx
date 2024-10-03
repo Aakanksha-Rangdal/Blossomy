b01c72b65d6671754ef7ea48962672c88e2cc0f4
Aakanksha-Rangdal <arrangdal@gmail.com>
2024-10-03 14:09:49 -0400

    Navigation options to wishlist and cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,43 @@ import Navbar from "./Navbar";
 import Box from "./Box";
 import Modal from "./Modal";
 import data from "../resources/inventory.json";
-// import useToggle from "./hooks/useToggle";
+import Wishlist from "./Wishlist";
+import Cart from "./Cart";
+// import Cart from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
-  // const { on, toggler } = useToggle();
   console.log(data, "data");
 
   return (
-    <div>
-      <Navbar />
-      <div className="grid grid-cols-4">
-        {data.map((item) => {
-          return <Box image={item.image} title={item.title} description={item.description} price={item.price} />;
-        })}
-        {/* <Box /> */}
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/Wishlist" element={<Wishlist />} />
+          <Route path="/Cart" element={<Cart />} />
+          {/* <Route path="/Home" element={<Home />} /> */}
+          <Route
+            path="/"
+            element={
+              <div className="grid grid-cols-4">
+                {data.map((item) => {
+                  return (
+                    <Box
+                      image={item.image}
+                      title={item.title}
+                      description={item.description}
+                      price={item.price}
+                    />
+                  );
+                })}
+              </div>
+            }
+          />
+        </Routes>
       </div>
-      {/* {on && <Modal toggler={toggler} />} */}
-      {/* <Modal /> */}
-    </div>
+    </Router>
   );
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,21 +4,23 @@ import home from './assets/home.png';
 import basket from './assets/basket.png';
 import heart from './assets/heart.png';
 import logo from './assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav className="navbar flex justify-between items-center bg-[#C8A1E0]">
       <div className="flex items-center cursor-pointer ">
           <img src={logo} alt="logo" className="w-52" />
       </div>
       <div className="flex items-center cursor-pointer">
-        <div className="mr-4">
+        <div className="mr-4" onClick={() => navigate('/Home')} >
             <img src={home} alt="home" className="home w-12 h-12" />
         </div>
-        <div className="mr-4 cursor-pointer">
+        <div className="mr-4" onClick={() => navigate('/Wishlist')}>
             <img src={heart} alt="heart" className="heart w-12 h-12" />
         </div>
-        <div className="mr-4 cursor-pointer">
+        <div className="mr-4 " onClick={() => navigate('/Cart')}>
             <img src={basket} alt="basket" className="basket w-12 h-12" />     
         </div>
       </div>
